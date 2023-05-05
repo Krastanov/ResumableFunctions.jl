60574faa6b94cee5ae562861901789c9b127e981
Gabriel Gerlero <gerlero@users.noreply.github.com>
2023-05-05 09:48:46 -0300

    Add support for delegation and return values with @yieldfrom (#8)
    ---------
    
    Co-authored-by: Stefan Krastanov <github.acc@krastanov.org>

--- src/Semicoroutines.jl
+++ src/Semicoroutines.jl
@@ -6,7 +6,7 @@ module Semicoroutines
   using MacroTools
   using MacroTools: combinedef, combinearg, flatten, postwalk
 
-  export @resumable, @yield, @nosave
+  export @resumable, @yield, @nosave, @yieldfrom 
 
   include("types.jl")
   include("transforms.jl")
